50d8171ef46a421411fc90c42ca99ef14bf229ab
TheCubiq <jlajsek@gmail.com>
2022-07-12 15:57:55 +0200

    updat App.js

--- App.js
+++ App.js
@@ -10,73 +10,171 @@ import {
 } from "react-native";
 import { colors } from "./src/constants";
 
-// generate random number between 6 and 17
-const generateRandomNumber = () => Math.floor(Math.random() * (17 - 6 + 1)) + 6;
-
 export default function App() {
-  const gridsize = 5;
-  const [columns, setColumns] = useState(
-    // randomly generate 2dimensional array with random numbers from 0 to 4
-    Array(gridsize)
+  const [GRIDSIZE] = useState(5);
+
+  const [grid, setGrid] = useState([]);
+
+  const createBoard = () => {
+    // generate grid with random numbers from 0 to 3
+    const board = Array(GRIDSIZE)
       .fill(0)
       .map(() =>
-        Array(gridsize)
+        Array(GRIDSIZE)
           .fill(0)
           .map(() => ({
-            color:Math.floor(Math.random() * 4), 
-            active:"false",
-          }
-          ))
-      )
-    // new Array(gridsize).fill(
-    //   new Array(gridsize).fill("2") // default content
-    //   randomly generate content with numbers from 0 to 4
-    //   new Array(gridsize).fill(Math.random(0, 4))
+            color: Math.floor(Math.random() * 4),
+            active: false,
+          }))
+      );
+      setGrid(board);
 
-    // )
-  );
+    // generate nice rainbow grid
+    // Array(GRIDSIZE)
+    //   .fill(0)
+    //   .map((_, i) =>
+    //     Array(GRIDSIZE)
+    //       .fill(0)
+    //       .map((_, j) => ({
+    //         color: (i + j)%4,
+    //         active: j === 0,
+    //       }))
+    //   )
+  };
 
-  const getNeighbors = (col, row) => {
+  const [demoText, setDemoText] = useState(0);
+
+  const demoButtonPress = () => {
+    console.log("demo button pressed");
+    setDemoText((demoText + 1) % 4);
+  };
+
+  const copyArray = (arr) => {
+    // return arr.map((row) => row.map((col) => col));
+    return [...arr.map((rows) => [...rows])];
+  };
+
+  const toggleTile = (row, col) => {
+    const updatedGrid = copyArray(grid);
+    const tile = updatedGrid[row][col];
+    tile.active = !tile.active;
+    setGrid(updatedGrid);
+  };
+
+  const updateGrid = () => {
+    const updatedGrid = copyArray(grid);
+
+    // for each active title
+    updatedGrid.forEach((row, i) => {
+      row.forEach((tile, j) => {
+        // check if tile is active
+        if (tile.active) {
+          // get neighbors of tile
+          const neighbors = getNeighbors(i, j);
+          // for each neighbor
+          // check if neighbor has the same color as the tile
+          neighbors.forEach((neighbor) => {
+            if (neighbor.color === tile.color)
+              // if so, set the neighbor to active
+              updatedGrid[neighbor.row][neighbor.col].active = true;
+          });
+        }
+      });
+    });
+
+    setGrid(updatedGrid);
+  };
+
+  const removeTile = (col, row) => {
+    const updatedGrid = copyArray(grid);
+    // delete the tile from this array using .filter
+    // console.log("\nbefore:", updatedGrid[row]);
+    updatedGrid[row] = updatedGrid[row].filter((tile, i) => i !== col);
+    // log the updated grid
+    console.log("removed!");
+    setGrid(updatedGrid);
+  };
+
+  const getNeighbors = (x, y) => {
     const neighbors = [];
-    if (row > 0) {
-      neighbors.push(columns[row - 1][col]);
+    if (x > 0) {
+      neighbors.push(grid[x - 1][y]);
     }
-    if (row < gridsize - 1) {
-      neighbors.push(columns[row + 1][col]);
+    if (x < GRIDSIZE - 1) {
+      neighbors.push(grid[x + 1][y]);
     }
-    if (col > 0) {
-      neighbors.push(columns[row][col - 1]);
+    if (y > 0) {
+      neighbors.push(grid[x][y - 1]);
     }
-    if (col < gridsize - 1) {
-      neighbors.push(columns[row][col + 1]);
+    if (y < GRIDSIZE - 1) {
+      neighbors.push(grid[x][y + 1]);
     }
-    // return neighbors color
-    return neighbors.map((neighbor) => neighbor.color);
+    // filter out undefined neighbors
+    return neighbors.filter((neighbor) => neighbor !== undefined);
   };
 
-  const isActive = (tile, j, i) => {
+  const checkActive = (x, y) => {
+    // get the color of the current tile
+    const tile = grid[x][y];
     // if cell is at the very bottom of the grid
-    if (tile.active === "true") {
+    if (y === 0) {
       return true;
     }
-    if (j === gridsize - 1) {
-      return true;
-    }
-    // if the neighbor of the color is active and has the same title as the current cell
-    
-    return false;
-  }	
+    // get the neighbors of the current tile
+    const neighbors = getNeighbors(x, y);
+    // check if there are neighbors that are active and have the same colors
+    const activeNeighbors = neighbors.filter(
+      (neighbor) => neighbor.active && neighbor.color === tile.color
+    );
 
-  const clickAction = (column, row,  tile) => {
-    // log the row, column and tile
-    // console.log(`row: ${row}, column: ${column}, tile: ${tile}`);
-    // get the neighbors of the clicked tile
-    const neighbors = getNeighbors(column,row);
-    // log the neighbors
-    console.log("neighbors:",neighbors);
-    // log active
-    console.log("active:",isActive(column,row));
+    // if there are active neighbors with the same color, return true
+    if (activeNeighbors.length > 0) {
+      return true;
+    }
+
+    // if the neighbor of the color is active and has the same title as the current cell
+
+    return false;
   };
+
+  const demoClickSwitch = (x, y) => {
+    switch (demoText) {
+      case 0:
+        toggleTile(x, y);
+        break;
+      case 1:
+        removeTile(y, x);
+        break;
+      case 2:
+        const neighbors = getNeighbors(x, y);
+        console.log(neighbors);
+        break;
+      case 3:
+        const isActive = checkActive(x, y);
+        console.log(`isActive: ${isActive}`);
+        break;
+    }
+  }	
+    }
+  };
+
+  const clickAction = (x, y) => {
+    // // log the row, column and tile
+    // toggleTile(x, y);
+    // removeTile(y, x);
+    const tile = grid[x][y];
+    console.log(
+      `column: ${x + 1}, row: ${y + 1}, tile: ${tile.color}, [${
+        tile.active ? "active" : "inactive"
+      }]`
+    );
+    demoClickSwitch(x, y);
+  };
+
+  useEffect(() => {
+    createBoard();
+  }, []);
+  
 
   return (
     <SafeAreaView style={[styles.container, styles.safearea]}>
@@ -84,31 +182,43 @@ export default function App() {
       <Text style={styles.title}>AQUAD</Text>
 
       <View style={styles.grid}>
+        {/* <GameGrid/> */}
         <View style={styles.map}>
-          {columns.map((column, i) => (
-            <View key={`collumn-${i}`} style={styles.column}>
-              {column.map((tile, j) => (
+          {grid.map((column, y) => (
+            <View key={`collumn-${y}`} style={styles.column}>
+              {column.map((tile, x) => (
                 <Pressable
-                  key={`tile-${j}-${i}`}
+                  key={`tile-${y}-${x}`}
                   style={[
                     styles.tile,
-                    { 
-                      // if the tile is active, set the background color to the primary color
-                      backgroundColor: isActive(tile, j, i) ? colors[`tile_${tile.color}`] : "transparent",
-                      borderColor: colors[`tile_${tile.color}`],                 
-                      },
+                    {
+                      backgroundColor: tile.active
+                        ? colors[`tile_${tile.color}`]
+                        : "transparent",
+                      borderColor: colors[`tile_${tile.color}`],
+                    },
                   ]}
                   onPress={() => {
-                    clickAction(j, i, tile);
+                    clickAction(y, x);
                   }}
                 >
-                  <Text style={styles.tile_text}>{tile.color}</Text>
+                  {/* <Text style={styles.tile_text}>{`${y+1}x${x+1}:${grid[y][x].color}`}</Text> */}
                 </Pressable>
               ))}
             </View>
           ))}
         </View>
       </View>
+
+      {/* simple test button */}
+      <Pressable
+        style={styles.btn}
+        onPress={() => {
+          demoButtonPress();
+        }}
+      >
+        <Text style={styles.tile_text}>{demoText}</Text>
+      </Pressable>
     </SafeAreaView>
   );
 }
