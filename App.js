d860e073f1d1d27f041a723a36d33cad69e4593d
TheCubiq <jlajsek@gmail.com>
2022-07-13 03:12:46 +0200

    update app.js

--- App.js
+++ App.js
@@ -1,10 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import {
-  Platform,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-} from "react-native";
+import { View, Platform, SafeAreaView, StyleSheet, Text } from "react-native";
 import GameBoard from "./src/components/GameBoard";
 import { colors } from "./src/constants";
 
@@ -13,7 +8,9 @@ export default function App() {
     <SafeAreaView style={[styles.container, styles.safearea]}>
       <StatusBar style="auto" />
       <Text style={styles.title}>AQUAD</Text>
-      <GameBoard />
+      <View style={styles.grid}>
+        <GameBoard />
+      </View>
     </SafeAreaView>
   );
 }
@@ -34,17 +31,7 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     color: colors.primary,
   },
-  map: {
-    // backgroundColor: "red",
-    alignSelf: "stretch",
-    // height: 100,
-    aspectRatio: 1,
-    flexDirection: "row",
-    // alignContent: "center",
-    // justifyContent: "center",
-    // padding: 10,
-    // flex:1,
-  },
+  
   column: {
     // backgroundColor: "blue",
     flex: 1,
@@ -58,42 +45,12 @@ const styles = StyleSheet.create({
     // alignItems: "center",
     // alignContent: "center",
   },
-  tile: {
-    // backgroundColor: "green",
-    transform: [
-      {
-        scale: 0.9,
-      },
-    ],
-    borderWidth: 8,
-    borderRadius: 10,
-
-    // margin: 5,
-
-    // i had to use transform scale instead of margin as the margin was
-    // not centering the tile correctly..
-    aspectRatio: 1,
-    alignSelf: "stretch",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  tile_text: {
-    fontSize: 32,
-    fontWeight: "bold",
-    color: colors.secondary,
-  },
   grid: {
     // backgroundColor: "#f700ff",
     flex: 1,
     alignSelf: "stretch",
-    padding: 10,
+    // padding: 10,
     justifyContent: "center",
     // alignItems: "center",
-  },
-  btn: {
-    backgroundColor: "#5a0059",
-    padding: 10,
-    borderRadius: 10,
-    margin: 10,
   },
 });
