5d68392d66de35bf2b9ff685b836da81b030f4b1
TheCubiq <jlajsek@gmail.com>
2022-07-13 03:16:18 +0200

    update GameBoard

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -3,17 +3,55 @@ import React from "react";
 
 import Tile from "./Tile";
 import Cell from "./Cell";
+import { Board } from "../objects";
 
-const Board = () => {
+const GameBoard = () => {
+  const [board, setBoard] = React.useState(new Board());
+
+  const cells = board.cells.map((row, rowIndex) => {
+    return (
+      <View key={rowIndex} style={styles.row}>
+        {row.map((col, colIndex) => {
+          return <Cell key={colIndex} cell={col} />;
+        })}
+      </View>
+    );
+  });
+
+  const copyBoard = (board) => {
+    return Object.assign(Object.create(Object.getPrototypeOf(board)), board);
+  };
+
+  const onTileClick = (tile) => {
+    if (board.hasWon()) {
+      return;
+    }
+    let newBoard = copyBoard(board).clicked(tile);
+    setBoard(newBoard); // update the board
+  };
+
+  const tiles = board.tiles
+    // .filter((tile) => tile.color !== 0)
+    .map((tile, index) => {
+      return <Tile key={index} tile={tile} onTileClick={onTileClick} />;
+    });
+
   return (
-    <View>
-      <Text>Board</Text>
-      <Tile />
-      <Cell />
+    <View style={styles.map}>
+      {/* {cells} */}
+      {tiles}
     </View>
   );
 };
 
-export default Board;
+export default GameBoard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    alignSelf: "stretch",
+    aspectRatio: 1,
+  },
+  row: {
+    flexDirection: "row",
+  },
+});
